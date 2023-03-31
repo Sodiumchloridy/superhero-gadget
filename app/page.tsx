64dec4ddd6d4b74cada6c6a4d51a0543a72bdec7
Sodiumchloridy <67376832+Sodiumchloridy@users.noreply.github.com>
2023-03-31 13:15:41 +0800

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <ImageSlider/>
       <div>
         <h2>Superhero Gadget - Low Price, High Value</h2>
-        <p>Looking for the latest and greatest mobile accessories? Look no further than Superhero Gadget! Our wide selection of high-quality phone cases, chargers, screen protectors, and more will keep your phone protected and running smoothly at an affordable price. With our commitment to low prices and high value, you can trust that you're getting the best deal possible. Shop now and see for yourself why Superhero Gadget is the go-to source for mobile accessories!</p>
+        <p>Looking for the latest and greatest mobile accessories? Look no further than Superhero Gadget! Our wide selection of high-quality phone cases, chargers, screen protectors, and more will keep your phone protected and running smoothly at an affordable price. With our commitment to low prices and high value, you can trust that youre getting the best deal possible. Shop now and see for yourself why Superhero Gadget is the go-to source for mobile accessories!</p>
       </div>
     </main>
   )
