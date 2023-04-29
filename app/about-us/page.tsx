695b761e85779ae8e5696a90d043db27c5e3e6ae
Sodiumchloridy <bryanlee13579@gmail.com>
2023-04-30 00:40:13 +0800

    updated files

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -6,35 +6,44 @@ import {useEffect} from 'react'
 export default function AboutUs(){
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     useEffect(() => {
-        const targets = document.querySelectorAll('.hacker') as NodeListOf<HTMLElement>;
-
-        let interval_iterations = 0;
-        const interval = setInterval(() => 
-        {
-            targets.forEach((target) => {
-                if (!target.textContent) return;
-                if (!target.dataset.value) target.dataset.value = target.textContent;
-                target.textContent = target.textContent.split('').map((letter, index) => {
-                    if (!target.dataset.value) return;
-                    if(index < interval_iterations) return `${target.dataset.value[index]}`
-                    if (letter !== ' ') {
-                        return `${letters[Math.floor(Math.random() * 26)]}`
-                    } else {
-                        return `${letter}`
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+              if (entry.isIntersecting) {
+                let interval_iterations = 0;
+                const interval = setInterval(() => 
+                {
+                    if (!target.textContent) return;
+                    if (!target.dataset.value) target.dataset.value = target.textContent;
+                    target.textContent = target.textContent.split('').map((letter, index) => {
+                        if (!target.dataset.value) return;
+                        if(index < interval_iterations) return `${target.dataset.value[index]}`
+                        if (letter !== ' ') {
+                            return `${letters[Math.floor(Math.random() * 26)]}`
+                        } else {
+                            return `${letter}`
+                        }
+                    }).join('') 
+                    
+                    if (interval_iterations >= target.dataset.value.length) {
+                        clearInterval(interval)
                     }
-                }).join('') 
-                
-                if (interval_iterations >= target.dataset.value.length) {
-                    clearInterval(interval)
-                }
 
-                interval_iterations += 1/3;
-            })
-        }, 30)
+                    interval_iterations += 1/3;
+                }, 30)
+              }
+            });
+          });
+
+        const target = document.querySelector('.hacker') as HTMLElement;
+        observer.observe(target);
+        return () => {
+            observer.unobserve(target);
+        };
     }, [])
     return(
         <PageWrapper>
             <main className="about-us">
+            <div className="flex-wrapper">
                 <h1 data-value="ABOUT US" className='hacker'>ABOUT US</h1>
                 <div className="section-wrapper">
                     <div className="drop-shadow-wrapper">
@@ -49,6 +58,7 @@ export default function AboutUs(){
                         </p>
                     </div>
                 </div>
+            </div>
                 <h2 className='motto'>Low Price, High Value</h2>
             </main>
         </PageWrapper>
