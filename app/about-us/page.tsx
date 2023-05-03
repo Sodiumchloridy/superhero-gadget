db55b951b3f1033d98319b91c7ef186140e01da7
Sodiumchloridy <bryanlee13579@gmail.com>
2023-05-03 12:49:01 +0800

    updated files

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -49,14 +49,12 @@ export default function AboutUs(){
                     <div className="drop-shadow-wrapper">
                         <img src="https://i.ibb.co/vz5WTfs/balkouras-nicos-5yl-Xkp-d-Zng-unsplash.webp" alt="" />
                     </div>
-                    <div className="text-wrapper">
-                        <p>
-                            Looking for the latest and greatest mobile accessories? Look no further than
-                            Superhero Gadget! We provide the latest and greatest gadgets and accessories to enhance your 
-                            mobile experience. Our mission is to offer high-quality products at an affordable 
-                            price so that everyone can enjoy the latest technology without breaking the bank.
-                        </p>
-                    </div>
+                    <p>
+                        Looking for the latest and greatest mobile accessories? Look no further than
+                        Superhero Gadget! We provide the latest and greatest gadgets and accessories to enhance your 
+                        mobile experience. Our mission is to offer high-quality products at an affordable 
+                        price so that everyone can enjoy the latest technology without breaking the bank.
+                    </p>
                 </div>
             </div>
                 <h2 className='motto'>Low Price, High Value</h2>
