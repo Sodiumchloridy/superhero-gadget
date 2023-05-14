3ac508a36a60e908d6e4e4fd4e2b390cb78e7b84
Sodiumchloridy <bryanlee13579@gmail.com>
2023-05-14 22:32:24 +0800

    updated files

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,17 +1,59 @@
 import { PageWrapper } from "@/components/PageWrapper/PageWrapper";
+import "./contact-us.css";
+import Link from "next/link";
 
 export default function ContactUs(){
     return(
         <PageWrapper>
-            <main className="main">
-                <h1>Contact Us</h1>
-                <p>23-G JALAN C180-1 DATARAN C180, Cheras, Selangor, Malaysia</p>
-                <p>03-9082-1091</p>
-                <p>We open daily from 10:30am to 10pm.</p>
-                <h2>Facebook</h2>
-                <p>https://www.facebook.com/superherogadgetmalaysia/</p>
-                <h2>Instagram</h2>
-                <p>https://www.instagram.com/superherogadget_c180/</p>
+            <main className="contact-us">
+                <iframe className="map" width="100%" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=23-G%20JALAN%20C180-1%20DATARAN%20C180,%20Cheras,%20Selangor,%20Malaysia+(Superhero%20Gadget)&amp;t=h&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                    <a href="https://www.maps.ie/distance-area-calculator.html">measure distance on map</a>
+                </iframe>
+                <div className="section-wrapper">
+                    <h1>Contact Us</h1>
+                    <div className="info-wrapper">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                            <path fill="white" d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
+                        </svg>
+                        <p>23-G JALAN C180-1 DATARAN C180, Cheras, Selangor, Malaysia</p>
+                    </div>
+                    <div className="info-wrapper">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path fill="white" d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/>
+                    </svg>
+                        <p>03-9082-1091</p>
+                    </div>
+                    <div className="info-wrapper">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path fill="white" d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/>
+                        </svg>
+                        <p>10:30am - 10pm</p>
+                    </div>
+                    <h2>Facebook</h2>
+                    <div className="info-wrapper">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path fill="white" d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/>
+                        </svg>
+                        <div>
+                            <p>Superhero Gadget</p>
+                            <Link style={{color: "red"}} href={'https://www.facebook.com/superherogadgetmalaysia/'}>
+                                <p>https://www.facebook.com/superherogadgetmalaysia/</p>
+                            </Link>
+                        </div>
+                    </div>
+                    <h2>Instagram</h2>
+                    <div className="info-wrapper">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                            <path fill="white" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
+                        </svg>
+                        <div>
+                            <p>superherogadget_c180</p>
+                            <Link style={{color: "red"}} href={'https://www.instagram.com/superherogadget_c180/'}>
+                                <p>https://www.instagram.com/superherogadget_c180/</p>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </main>            
         </PageWrapper>        
     )
--- app/outlets/page.tsx
+++ app/outlets/page.tsx
@@ -1,24 +1,46 @@
-import styles from './page.module.css'
+import './outlets.css'
 import { PageWrapper } from '@/components/PageWrapper/PageWrapper';
 
 export default function Outlets(){
     return(
         <PageWrapper>
-            <div className='main'>
-                <h1>Outlets</h1>
-                <div className={styles.grid}>
-                    <div className={styles.outlet}>
+            <main className='outlets'>
+                <div className="title">
+                    <div>
+                        <h1>Outlets</h1>
+                        <div className="underscore"></div>
+                    </div>
+                </div>
+                <div className="grid">
+                    <div className="outlet">
                         <h2>C180 Branch</h2>
-                        <img className={styles.img} src="/images/store-front.webp" alt="C180 Branch" />
+                        <img className="img" src="/images/store-front.webp" alt="C180 Branch" />
                         <iframe 
-                            className={styles.maps} 
+                            className="maps" 
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.2209216454244!2d101.7639032152523!3d3.0353234546870995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc350bb0a4fa47%3A0x5e3f7ae6c6d95d7f!2sSuperhero%20Gadget!5e0!3m2!1sen!2smy!4v1680237878234!5m2!1sen!2smy" 
                             loading="lazy"
                         ></iframe>
-                        <p>23-G JALAN C180-1 DATARAN C180, Cheras, Selangor, Malaysia</p>
+                        <div className="info-wrapper">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                                <path fill="white" d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
+                            </svg>
+                            <p>23-G JALAN C180-1 DATARAN C180, Cheras, Selangor, Malaysia</p>
+                        </div>
+                        <div className="info-wrapper">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path fill="white" d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/>
+                            </svg>
+                            <p>03-9082-1091</p>
+                        </div>
+                        <div className="info-wrapper">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path fill="white" d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/>
+                            </svg>
+                            <p>10:30am - 10pm</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </main>
         </PageWrapper>
         
     )
